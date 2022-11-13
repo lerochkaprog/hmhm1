3c53262880b7373ed69df6ac6da910029be956a8
MOB_master <lera061009@gmail.com>
2022-11-13 12:25:55 +0300

    hmm

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ def divider(a, b):
         raise ValueError
     if b > 100:
         raise IndexError
-        return a/b
+    if type(a) == "str" or type(a) == 'type':
+        raise TypeError
+
+    return a/b
+
+
 data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
 for key in data:
-    res = divider(key, data[res])
+    res = divider(key, data[key])
     result.append(res)
+    break
 
 print(result)
